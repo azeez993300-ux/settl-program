acb6938d6f5e783ef8690f7fbb4a3cacbf57291d
azeez993300-ux <azeez993300@gmail.com>
2026-05-13 18:04:52 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,15 +1,1 @@
-# 1. First, remove the nested .git folder (this is critical)
-rm -rf settl/.git
 
-# 2. Move all files from nested folder to main (including hidden files)
-mv settl/* . 2>/dev/null
-mv settl/.[!.]* . 2>/dev/null  # Moves hidden files like .env
-
-# 3. Remove the now-empty nested directory
-rmdir settl
-
-# 4. Stage all changes
-git add .
-
-# 5. Check what's staged
-git status
